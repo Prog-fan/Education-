e68852a695c735a089c118401e92b457c685da22
Prog-fan <programootka@gmail.com>
2024-10-21 17:01:14 +0500

    release

---
+++ Stopwatch/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Stopwatch : Form
+    {
+        public Stopwatch()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrSecundomer_Tick(object sender, EventArgs e)
+        {
+            DateLabel.Text = DateTime.Now.ToString();
+
+            int tmp = Int32.Parse(txtSeconds.Text); tmp++;
+            txtSeconds.Text = tmp.ToString();
+
+            if (txtSeconds.Text == "60")
+            {
+                txtSeconds.Text = "0";
+                int tmm = Int32.Parse(txtMinutes.Text); tmm++;
+                txtMinutes.Text = tmm.ToString();
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (tmrSecundomer.Enabled == true)
+            {
+                btnStart.Text = "Start";
+                ClearButton.Enabled = true;
+                tmrSecundomer.Enabled = false;
+            }
+            else
+            {
+                tmrSecundomer.Enabled = true;
+                ClearButton.Enabled = false;
+                btnStart.Text = "Stop";
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearButton.Enabled = false;
+            txtMinutes.Text = "0";
+            txtSeconds.Text = "0";
+        }
+
+        private void TimerButton_Click(object sender, EventArgs e)
+        {
+            Stopwatch.ActiveForm.Hide();
+            Timer timer = new Timer();
+            timer.ShowDialog();
+            Close();
+        }
+    }
+}
---
+++ Stopwatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Stopwatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stopwatch")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f58d2c9e-6bd0-45a2-b550-0c149fb5c8ba")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
